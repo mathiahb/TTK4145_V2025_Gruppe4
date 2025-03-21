8892140d2d392a39ab265bbf940cac2f36c596f0
Åsne <asneomland@gmail.com>
2025-03-21 15:55:52 +0100

    moved chanels that communicate with the shared state module to shared state

--- Project/elevator/elevator_logic.go
+++ Project/elevator/elevator_logic.go
@@ -1,92 +1,64 @@
 package elevator
 
 import (
-	"elevator_project/shared_states"
+	//. "elevator_project/constants" 
 	"elevator_project/elevio"
-	"fmt"
+	"elevator_project/shared_states"
 )
 
-
 type ElevatorChannels struct { // channels and threads for communication within the different parts of the elevator
-	Button chan elevio.ButtonEvent // okei, så problemet har vært med importering av elevio
-	Floor chan int
-	Obstruction chan bool
+	Button        chan elevio.ButtonEvent // okei, så problemet har vært med importering av elevio
+	Floor         chan int
+	Obstruction   chan bool
 	DoorTimerIsUp chan bool
-	
 }
 
-func MakeElevatorChannels() ElevatorChannels{
+func MakeElevatorChannels() ElevatorChannels {
 	return ElevatorChannels{
-		Button: make(chan elevio.ButtonEvent),
-		Floor: make(chan int),
-		Obstruction: make(chan bool),
+		Button:        make(chan elevio.ButtonEvent),
+		Floor:         make(chan int),
+		Obstruction:   make(chan bool),
 		DoorTimerIsUp: make(chan bool),
 	}
 }
 
-type BetweenElevatorAndSharedStatesChannels struct {
-	HallRequestChannel chan shared_states.HRAType
-	ElevatorStateChannel chan Elevator
-	ClearCabRequestChannel chan Elevator
-	ClearHallRequestChannel chan HallRequestsType
-	ApprovedClearHallRequestsChannel chan HallRequestsType
-	NewHallRequestChannel chan HallRequestsType
-	ApprovedHallRequestChannel chan HallRequestsType
-
-}
-
-func MakeBetweenElevatorAndSharedStatesChannels() BetweenElevatorAndSharedStatesChannels {
-	return BetweenElevatorAndSharedStatesChannels {
-		HallRequestChannel: make(chan shared_states.HRAType), // fra shared state til elevator
-		ElevatorStateChannel: make(chan Elevator),        // fra elevator til shared states
-		ClearCabRequestChannel: make(chan Elevator),
-		ClearHallRequestChannel: make(chan HallRequestsType),
-		ApprovedClearHallRequestsChannel: make(chan HallRequestsType),
-		NewHallRequestChannel: make(chan HallRequestsType), // fra elevator til shared states, sender ny HallRequest, når knapp trykket inn
-		ApprovedHallRequestChannel: make(chan HallRequestsType), // fra shared state til elevator, sender godkjent HallRequest etter konferering med nettverket
-		
-	}
-}
-
-
 // må legge til alle kanalene
-func ElevatorThread(elevatorChannels ElevatorChannels, betweenElevatorAndSharedStatesChannels BetweenElevatorAndSharedStatesChannels){
+func ElevatorThread(elevatorChannels ElevatorChannels, betweenElevatorAndSharedStatesChannels shared_states.BetweenElevatorAndSharedStatesChannels) {
 	//føler at det er litt initialisering/konfigurering som mangler
 
-	var localElevator = ElevatorUninitialized() // lager et lokalt heisobjekt
+	var localElevator = ElevatorUninitialized()    // lager et lokalt heisobjekt
 	var hallRequests = HallRequestsUninitialized() // lager et tomt request-objekt
 	var isObstructed = false
 
 	localElevator = InitFSM(elevatorStateChannel, localElevator) // shared state får vite at en heis eksisterer, kjenner ikke helt til poenget med resten av funksjonen
-	if (inputDevice.FloorSensor() == -1) { // fase ut inputDevice?
+	if inputDevice.FloorSensor() == -1 {                         // fase ut inputDevice?
 		localElevator = FSMOnInitBetweenFloors(localElevator, elevatorStateChannel)
 	}
-	
+
 	for {
-		select{
-		
-		case buttonEvent := <-buttonChannel: 
-			localElevator = FSMButtonPress(buttonEvent.btnFloor,  buttonEvent.btnType, localElevator, elevatorStateChannel, newHallRequestChannel)
-		
-		case newFloor := <-floorsChannel:
+		select {
+
+		case buttonEvent := <- Button:
+			localElevator = FSMButtonPress(buttonEvent.btnFloor, buttonEvent.btnType, localElevator, elevatorStateChannel, newHallRequestChannel)
+
+		case newFloor := <- Foor:
 			localElevator, hallRequests = FSMOnFloorArrival(newFloor, localElevator, hallRequests, clearHallRequestChannel, clearCabRequestChannel, elevatorStateChannel)
 
-		case isObstructed = <- obstructionChannel: // dette føles rart, men er nødt til å vite om noen prøver å obstructe døren
-			
-			if(isObstructed){
+		case isObstructed = <- Obstruction: // dette føles rart, men er nødt til å vite om noen prøver å obstructe døren
+
+			if isObstructed {
 
 			}
-		
-		case <- doorTimerIsUp:
 
-			if(!isObstructed){
+		case <-doorTimerIsUp:
+
+			if !isObstructed {
 				localElevator = FSMCloseDoors(localElevator, hallRequests, elevatorStateChannel)
 			}
 
 		case hallRequests <- approvedHallRequestChannel: // fordi alle ordre kommer fra shared states
 			localElevator = FSMStartMoving(localElevator, hallRequests, elevatorStateChannel)
-			
+
 		}
 	}
 }
-
