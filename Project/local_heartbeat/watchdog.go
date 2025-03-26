1e3d71aab59ceb2b04985a81e2203cd232656fe5
Åsne <asneomland@gmail.com>
2025-03-26 12:00:12 +0100

    changed todo file and removed unused folders

--- Project/local_heartbeat/watchdog.go
+++
@@ -1,4 +0,0 @@
-package local_heartbeat
-
-// Implements a dual system communicating existence to each other.
-// 3 missed heartbeats and assume program is hung -> PKill -> Takeover -> Spawn new watchdog.
